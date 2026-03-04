f2ce846724d6d465ce776c4adecb19538a575480
EH Admin <jermie@effortlesshome.co>
2026-03-04 18:18:45 -0500

    latest

--- custom_components/EffortlessHome/switch.py
+++ custom_components/EffortlessHome/switch.py
@@ -50,6 +50,7 @@ async def async_setup_entry(
             SleepModeSwitch(),
             MotionNotificationsSwitch(),
             MonitoringAlarmSwitch("monitoringalarm"),
+            DisableMotionLightingSwitch(),
             SmartApplianceConversionSwitch("SmartAppliance1"),
             SmartApplianceConversionSwitch("SmartAppliance2"),
             SmartApplianceConversionSwitch("SmartAppliance3"),
@@ -240,4 +241,57 @@ class PresenceSimulationSwitch(SwitchEntity, RestoreEntity):
         if (last_state := await self.async_get_last_state()) is not None:
             self._is_on = last_state.state == "on"
 
-    
+class DisableMotionLightingSwitch(SwitchEntity, RestoreEntity):
+    """Set up a motion lighting switch."""
+
+    _attr_should_poll = False
+
+    def __init__(self, hass: HomeAssistant) -> None:
+        """Initialize switch."""
+        self._is_on: bool = False
+        self.hass = hass
+        self.name = "Disable Motion Lighting"
+        self.entity_id = "switch.motion_lighting_disable"
+
+    @property
+    def device_info(self):
+        """Return information about the device."""
+        return {
+            "identifiers": {(DOMAIN, NAME)},
+            "name": NAME,
+            "manufacturer": NAME,
+        }
+
+    @cached_property
+    def name(self) -> str | UndefinedType | None:
+        """Return the name of the entity."""
+        return self.name
+
+    @cached_property
+    def device_class(self) -> SwitchDeviceClass | None:
+        """Return device class."""
+        return SwitchDeviceClass.SWITCH
+
+    @property
+    def is_on(self) -> bool | None:
+        """Return the state of the switch."""
+        return self._is_on
+
+    def turn_on(self, **kwargs) -> None:
+        """Turn on switch."""
+        self._is_on = True
+        self.schedule_update_ha_state()
+
+    def turn_off(self, **kwargs):
+        """Turn off switch."""
+        self._is_on = False
+        self.schedule_update_ha_state()
+
+    async def async_added_to_hass(self):
+        """Restore previous state when entity is added."""
+        await super().async_added_to_hass()
+
+        if (last_state := await self.async_get_last_state()) is not None:
+            self._is_on = last_state.state == "on"
+
+        
